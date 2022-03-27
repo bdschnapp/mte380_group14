f7c05521b705186bf220374d35ba93ff37a84805
Max <mtwinter73@gmail.com>
2022-03-27 08:47:55 -0400

    addressed comments, increased linear Kp to run faster while maintaining stability

--- test_linear_controller.cpp
+++ test_linear_controller.cpp
@@ -7,7 +7,7 @@
 namespace test_linear_controller
 {
     constexpr auto TARGET_DISTANCE = 0.21f;
-    const float Kp = 100;
+    const float Kp = 150;
     const float Ki = 3;
     controllers::linear_controller lc(Kp, Ki, DISTANCE_TOLERANCE);
     actuator::TB9051FTG motor_driver;
